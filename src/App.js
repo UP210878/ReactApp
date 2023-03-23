9b731a1b7e04ad6cf27ad15ac1021ba2ce2fb05e
UP210878 <Up210878@alumnos.upa.edu.mx>
2023-03-23 00:26:53 -0600

    New changes, app, pages developed

--- src/App.js
+++ src/App.js
@@ -5,31 +5,40 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import "./App.css"
+import {MainPage,
+ContactPage,
+AboutPage,
+PricingPage} from './MainPage.js'
+import 'bootstrap'
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark py-4">
+          <ul class="navbar-nav sm-icons">
+          <Link to="/" class= "navbar-brand">New Product</Link>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" class= "nav-link">Home</Link> 
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" class= "nav-link"> About</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/pricing" class= "nav-link"> Pricing</Link>
+            </li>
+            <li>
+              <Link to="/contact" class= "nav-link"> Contact</Link>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
           <Route path="/about" element= {<About />} />
-          <Route path="/users" element={<Users />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
       </div>
     </Router>
@@ -37,14 +46,17 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return <div><MainPage></MainPage></div>;
 }
 
 function About() {
-  return <h2>About</h2>;
+  return <h2><AboutPage></AboutPage></h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function Contact() {
+  return <h2><ContactPage></ContactPage></h2>;
 }
 
+function Pricing() {
+  return <h2><PricingPage></PricingPage></h2>;
+}
