062173eca83be8db44b5d562f4d2187096c4762d
UP210878 <Up210878@alumnos.upa.edu.mx>
2023-03-26 23:59:41 -0600

    Changed UI for homepage

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,22 +2,32 @@ import "bootstrap";
 
 const Title = (pagina) => {
   return (
-    <><div style={{
-      backgroundImage: `url("https://scitechdaily.com/images/Black-Hole-Event-Horizon-Artistic-Illustration.jpg")`}}>
-        <h1 class="bg-image shadow-2-strong mask"
+    <>
+      <div
+        class="bg-image shadow-2-strong mask"
+        style={{
+          backgroundImage: `url("https://i.pinimg.com/originals/67/18/22/671822c2f63dd5f65d8fd15c9710420b.jpg")`,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1
           style={{
+            display: "inline-block",
             color: "black",
             textShadow:
               "2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff, 1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff",
-            fontSize: "900%",
-            textAlign: 'center',
-            paddingBlock: '10px',
+            fontSize: "800%",
+            textAlign: "center",
+            paddingBlock: "10px",
           }}
         >
           {pagina}
-        </h1></div>
+        </h1>
+      </div>
     </>
   );
-}
+};
 
 export default Title;
