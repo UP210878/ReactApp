ab940ce27c697139eae2a27676020c6dc50b96f8
UP210305 <up210305@alumnos.upa.edu.mx>
2023-03-29 19:24:55 -0600

    Upload file of Home

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container color_text_Content">
+      <div className="row">
+        <div className="col-6">
+          <div className="content_Text_Subtitle">
+            <p className="home.heading.Sub">SORRY WE'RE CURRENTLY DOWN</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="content_Text_Title">
+            <p className="home.heading.Title">
+            WE'RE IMPROVING OUR 
+            WIRES TO GIVE YOU A 
+            BETTER EXPERIENCE
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="circle">
+        
+      </div>
+    </div>
+    
+  );
+};
+
+export default Home;
