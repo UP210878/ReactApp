26a7817be7793a4ec7deb41fbb22911c4afac941
UP210305 <up210305@alumnos.upa.edu.mx>
2023-03-29 19:23:12 -0600

    Text of Home added

--- src/Content/CurrentlyDown.js
+++ src/Content/CurrentlyDown.js
@@ -1,11 +1,12 @@
 import { sendEmailButton, linkedinButton } from "../components/Buttons";
+import Home from "../components/Home";
 
 function mainPage() {
   return (
     <><p style={{fontSize:'500%'}}>{language === "en"?"Hi":"Que ondas"}</p>
       <div class="pt-5">
         <section class="p-5 mt-5">
-{/* Texto va aqui */}
+        {Home()}
         </section>
         <div class="px-5 mx-5 space-x-3">
           {sendEmailButton()} {linkedinButton()}
